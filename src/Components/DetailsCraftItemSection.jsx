ae7880f98bb4ccae6a0ed2f2545ed16795286e5c
RafiulAlamRami <rafiul7368@gmail.com>
2024-04-30 20:47:22 +0600

    update detailes page design

--- src/Components/DetailsCraftItemSection.jsx
+++ src/Components/DetailsCraftItemSection.jsx
@@ -9,7 +9,7 @@ const DetailsCraftItemSection = () => {
     
     return (
         <div>
-            <h1 className="text-[2rem] font-bold border-b-2 pb-[1em]">Detailes of {craft.itemName}</h1>
+            <h1 className="text-[2rem] font-bold my-[3em] text-center">Detailes of <span className='border-b-2 border-b-black pb-[.3em]'>{craft.itemName}</span></h1>
             <div className="hero min-h-screen bg-base-200">
                 <div className="rounded-lg hero-content flex-col md:flex-row lg:flex-row justify-between border-4 gap-[3em]">
                     <div>
