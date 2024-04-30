e51dcb60610d156ff3bb5ec28af45fd5a0e96bdc
RafiulAlamRami <rafiul7368@gmail.com>
2024-04-30 21:51:31 +0600

    faq section create

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <footer className="footer footer-center p-10 bg-primary text-primary-content flex justify-around">
+            <footer className="footer footer-center p-10 bg-primary text-primary-content flex justify-around items-center">
                 <aside>
                     
                     <p className="font-lex text-[2rem] font-bold">
@@ -30,20 +30,20 @@ const Footer = () => {
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 mr-2 sm:mr-6">
 						<path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
 					</svg>
-					<span>Lake City,Dhaka-1200</span>
+					<span className='text-[1.3rem]'>Lake City,Dhaka-1200</span>
 				</p>
 				<p className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 mr-2 sm:mr-6">
 						<path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
 					</svg>
-					<span>123456789</span>
+					<span className='text-[1.3rem]'>123456789</span>
 				</p>
 				<p className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 mr-2 sm:mr-6">
 						<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
 						<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
 					</svg>
-					<span>contact@fabby.com</span>
+					<span className='text-[1.3rem]'>contact@fabby.com</span>
 				</p>
 			</div>
 		</div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from './Slider';
 import CraftItemSection from './CraftItemSection';
 import CategoryCraft from './CategoryCraft';
+import Faq from './Faq';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Slider></Slider>
             <CraftItemSection></CraftItemSection>
             <CategoryCraft></CategoryCraft>
+            <Faq></Faq>
         </div>
     );
 };
